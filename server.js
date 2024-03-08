1d37147b1c13c65a775c6e036252f77fb4201176
Adekunle Adesokan <kunle.adesokan9@gmail.com>
2024-03-08 10:45:01 +0100

    add frontend build file

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const persons = require("./persons.js");
 const { formattedDate, deleteItem, addItem } = require("./helpers.js");
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static("dist"));
 
-// app.use(morgan("tiny"));
+app.use(cors());
 
 app.use(
   morgan((tokens, req, res) => {
